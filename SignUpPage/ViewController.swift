6987e965d2cdf24bc72e9ebc110595ae1f960e77
Arashdeep Kaur <official.arashdeep96@gmail.com>
2022-03-01 08:25:41 -0500

    Initial Commit

---
+++ SignUpPage/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SignUpPage
+//
+//  Created by user206151 on 3/1/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
